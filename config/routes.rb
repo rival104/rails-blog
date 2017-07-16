35a8cba2763b363dfc3ca7ad42770080089927d6
rival104 <navid.rvl@gmail.com>
2017-07-16 23:45:16 +0000

    add create user from gui

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create' #alternative to resources
+  resources :users, except: [:new]
+  
   
   
   
