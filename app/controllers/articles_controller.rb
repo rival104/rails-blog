a6ff9ce826f5161c73182a8644fd815968c95e3d
rival104 <navid.rvl@gmail.com>
2017-07-18 03:42:12 +0000

    add pagintaion to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
-    @articles = Article.all # @artilcles can be named anything.
+    @articles = Article.paginate(:page => params[:page], :per_page => 5) # @artilcles can be named anything.
   end
   
   def new
